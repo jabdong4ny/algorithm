2890b250fe408d2866a68f705d63c7d27ed7a216
jabdong4ny <jabdong4ny@gmail.com>
2023-05-05 23:38:00 +0900

    [JadenCase 문자열 만들기] [문제] : 공백이 연속으로 나올 때 런타임에러 발생 [대책] - 문장앞에 공백 예외처리 추가 - 문장 끝에 공백이 나올 때 공백처리 추가

--- programmers/level2/JadenCase.java
+++ programmers/level2/JadenCase.java
@@ -2,14 +2,27 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         String[] temp = s.split(" ");
-        //System.out.println(temp.length);
+        
         for(int i = 0; i< temp.length ;i++){
+            //문장앞에 공백이 있는 경우 substring으로 접근하면서 에러가 발생하기에 별도로 처리가 필요하다.
+            if(temp[i].isEmpty()){
+              answer = answer + " ";
+                continue;  
+            }
             if(i == 0)
-                answer = temp[i].substring(0,1).toUpperCase() + temp[i].substring(1).toLowerCase() + " ";
-            else if(i == (temp.length-1))
-                answer = answer + temp[i].substring(0,1).toUpperCase() + temp[i].substring(1).toLowerCase();
+                answer = temp[i].substring(0,1).toUpperCase() + temp[i].substring(1).toLowerCase();
             else
-                answer = answer + temp[i].substring(0,1).toUpperCase() + temp[i].substring(1).toLowerCase() + " ";
+                answer = answer + temp[i].substring(0,1).toUpperCase() + temp[i].substring(1).toLowerCase();
+            
+            if(i != (temp.length-1)) answer = answer + " ";
+        }
+        
+        //문장의 뒤에 공백이 있을 경우 공백을 추가해줌
+        if(s.length() > answer.length()){
+            int sub = s.length() -answer.length();
+            for(int j=0;j < sub;j++){
+                answer += " "; 
+            }            
         }
 
         return answer;
