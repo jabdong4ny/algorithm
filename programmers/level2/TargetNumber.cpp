de88e02f31c278926bee71b7def4e88feb9e9055
jabdong4ny <jabdong4ny@gmail.com>
2023-06-27 14:50:35 +0900

    [ 깊이/너비 우선 탐색(DFS/BFS) ][타켓 넘버/C++] *   BFS case 추가 *   DFS 로직에 주석처리되어 있음 *   상세설명 : https://jabdon4ny.tistory.com/74

--- programmers/level2/TargetNumber.cpp
+++ programmers/level2/TargetNumber.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -20,13 +22,51 @@ void DFS(int index, int sum)
     DFS(index+1, sum - gNumbers[index]);
 }
 
+void BFS() {
+    vector<int> temp_sum, sum;
+    int sum_counter = 0;
+    double sum_result_size = pow(2, numbers_size);
+    temp_sum.resize(sum_result_size, 0);
+    sum.resize(sum_result_size, 0);
+
+    //idx 0
+    sum[sum_counter++] = + gNumbers[0];
+    sum[sum_counter++] = - gNumbers[0];
+
+
+    //idx 1~ numbers size
+    for (int numInx = 1; numInx < numbers_size; numInx++) {
+        temp_sum = sum;
+        sum_counter = 0;
+        double recurse = pow(2, numInx);
+        for (int i = 0; i < recurse ; i++) {
+            sum[sum_counter++] = temp_sum[i] + gNumbers[numInx];
+            sum[sum_counter++] = temp_sum[i] - gNumbers[numInx];
+        }
+        cout << "sum_counter : " << sum_counter << " numInx : " << numInx << endl;
+    }
+
+    for (int numInx = 0; numInx < sum_result_size; numInx++) {
+        if (g_target == sum[numInx]) {
+            answer++;
+        }
+    }
+    return;
+}
+
 int solution(vector<int> numbers, int target) {
     answer = 0;
     gNumbers = numbers;
     g_target = target;
     numbers_size = numbers.size();
-
-    DFS(0,0);
+    //DFS(0,0);
+    BFS();
     
     return answer;
+}
+
+int main()
+{
+    vector<int> numbers = {1, 1, 1, 1, 1};
+    cout << solution(numbers,3);
 }
